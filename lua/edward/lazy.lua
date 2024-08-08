27a6bcf4fe35b563b2b55c406a002ea750b68b9b
Edward <codedwardwong@gmail.com>
2024-08-08 23:43:53 +0800

    +add lsp, formatting, gitsigns, lazygit

--- lua/edward/lazy.lua
+++ lua/edward/lazy.lua
@@ -11,13 +11,12 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("edward.plugins",{
-checker={
-enable = true,
-notify= false
-},
-
-change_detection = {
-  notify = false,
-}
+require("lazy").setup({ { import = "edward.plugins" }, { import = "edward.plugins.lsp" } }, {
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  change_detection = {
+    notify = false,
+  },
 })
