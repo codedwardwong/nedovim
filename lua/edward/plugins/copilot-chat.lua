b4fcf07a4dd97250be5e54910f4780b4db2f30d7
Edward Wong <itdeka@GM1027M0023.local>
2024-09-09 18:35:59 +0800

    +add copilot chat and copilot

---
+++ lua/edward/plugins/copilot-chat.lua
@@ -0,0 +1,68 @@
+return {
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		branch = "canary",
+		dependencies = {
+			{ "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+			{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+		},
+		build = "make tiktoken", -- Only on MacOS or Linux
+		opts = {
+			debug = true, -- Enable debugging
+			-- See Configuration section for rest
+		},
+		-- See Commands section for default commands if you want to lazy load on them-- lazy.nvim keys
+
+		-- Quick chat with Copilot
+		event = "VeryLazy",
+		keys = {
+			{
+				"<leader>cch",
+				function()
+					local actions = require("CopilotChat.actions")
+					require("CopilotChat.integrations.telescope").pick(actions.help_actions())
+				end,
+				desc = "CopilotChat - Help actions",
+			},
+			-- Show prompts actions with telescope
+			{
+				"<leader>ccp",
+				function()
+					local actions = require("CopilotChat.actions")
+					require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
+				end,
+				desc = "CopilotChat - Prompt actions",
+			},
+			-- Code related commands
+			{ "<leader>cce", "<cmd>CopilotChatExplain<cr>", desc = "CopilotChat - Explain code" },
+			{ "<leader>cct", "<cmd>CopilotChatTests<cr>", desc = "CopilotChat - Generate tests" },
+			{ "<leader>ccr", "<cmd>CopilotChatReview<cr>", desc = "CopilotChat - Review code" },
+			{ "<leader>ccR", "<cmd>CopilotChatRefactor<cr>", desc = "CopilotChat - Refactor code" },
+			{ "<leader>ccn", "<cmd>CopilotChatBetterNamings<cr>", desc = "CopilotChat - Better Naming" },
+			-- Chat with Copilot in visual mode
+			{
+				"<leader>ccv",
+				":CopilotChatVisual",
+				mode = "x",
+				desc = "CopilotChat - Open in vertical split",
+			},
+			{
+				"<leader>ccx",
+				":CopilotChatInline<cr>",
+				mode = "x",
+				desc = "CopilotChat - Inline chat",
+			},
+			-- Custom input for CopilotChat
+			{
+				"<leader>cci",
+				function()
+					local input = vim.fn.input("Ask Copilot: ")
+					if input ~= "" then
+						vim.cmd("CopilotChat " .. input)
+					end
+				end,
+				desc = "CopilotChat - Ask input",
+			},
+		},
+	},
+}
